2e9246a52f9816b9d4b14d0289c6dd97f464d519
Ivan <2350327610@qq.com>
2023-04-25 17:11:33 +0800

    YihuanXu First commit

--- index.php
+++ index.php
@@ -4,6 +4,38 @@
     require_once "a5common/commonUI.php";
 ?>
 
+
+<?php
+
+//if (isset($_SESSION['user_id'])) {
+//    header("Location: a5product.php");
+//    exit;
+//}
+
+
+$error = "";
+if (isset($_POST['login'])) {
+    $username = mypost('username');
+    $password = mypost('password');
+
+    $stmt = $con->prepare("SELECT Admin_Password FROM admin WHERE Admin_ID = ?");
+    $stmt->bindParam(1, $username, PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($result['Admin_Password'] != null) {
+            if ($password == $result['Admin_Password']) {
+                $_SESSION['user_id'] = $username;
+                header("Location: a5product.php");
+                exit;
+            } else {
+                $error = "Incorrect username or password.";
+            }
+    } else {
+        $error = "User does not exist.";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,12 +57,45 @@
     <title>CAN302 A5 store | Category</title>
 </head>
 <body>
-    <!-- 主内容 -->
-    <div class="content">
-        <div 
-        style="width: 40px;height:30px;border:1px solid black;margin:auto;cursor:pointer;"
-        onclick="location.href='a5product.php'">Login</div>
-    </div>
+    <!-- 顶部框 -->
+    <?php
+    topbarUI();
+    ?>
+
+    <?php
+        echo '
+        <div class="mdui-card">
+            <div class="mdui-card-media">
+                <img src="WechatIMG9.jpeg"/>
+            </div>
+            
+            <div class="mdui-card-primary-title">Admin System</div>
+            
+            <div class="mdui-card-content" id="table"> 
+                <form class="form-online" role="form" action="index.php" method="post">
+                    <div class="mdui-textfield mdui-textfield-floating-label">
+                        <label class="mdui-textfield-label">Username</label>
+                        <input id="username" name="username" class="mdui-textfield-input"/>
+                    </div>
+                    <div class="mdui-textfield mdui-textfield-floating-label">
+                        <label class="mdui-textfield-label">password</label>
+                        <input id="password" name="password" class="mdui-textfield-input"/>
+                    </div>
+                    <button type="submit" class="btn btn-primary" id="login" name="login" value="login"> Login </button>
+                </form>
+                
+                
+                
+            </div>     
+        </div>
+        ';
+    ?>
+    <?php if (!empty($error)): ?>
+        <p><?php echo $error; ?></p>
+    <?php endif; ?>
+
+
+
 </body>
 <!-- 这个页面的JS，放在文档尾部 -->
 <script src="a5common/commonJS.js"></script>
