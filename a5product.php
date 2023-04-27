d8a085454f22707b6d19e10b332149c3bb37280c
kikipotter82 <120178964+kikipotter82@users.noreply.github.com>
2023-04-27 13:09:47 +0800

    Update a5product.php

--- a5product.php
+++ a5product.php
@@ -1,6 +1,43 @@
 <?php
     require_once "a5common/commonPHP.php";
     require_once "a5common/commonUI.php";
+
+        // 定义查询语句，只找deleted字段为TRUE的那些数据
+        $productsql='SELECT * FROM product WHERE `deleted`= FALSE';
+        // 定义表头数据，这个名字最好不要重复，比如user页面的表头的变量名为userhead，那么product页面的这个变量名就可以是producthead
+        //Product_ID	Product_Name	Product_Category_ID	Product_In_stock	
+        //Product_Price	Product_Description	Product_Image_link
+        $producthead=' 
+                    <th>Product ID</th>
+                    <th>Product Name</th>
+                    <th>Category ID</th>
+                    <th>In Stock</th>
+                    <th>Price</th>
+                    <th>Description</th>
+                    <th>Image</th>
+                    ';
+
+        function productrender($row){
+            $rowid=$row["Product_ID"];
+
+            echo "<td>".$row["Product_ID"]."</td>";
+            echo "<td>".$row["Product_Name"]."</td>";
+            echo "<td>".$row["Product_Category_ID"]."</td>";//改成显示name
+            echo "<td>".$row["Product_In_stock"]."</td>";
+            echo "<td>".$row["Product_Price"]."</td>";
+            echo "<td>".$row["Product_Description"]."</td>";
+            echo "<td><img src='".$row["Product_Image_link"]."' alt='".$row["Product_Name"]."' style='max-width: 100px;'></td>";
+            echo "<td>
+                <button onclick='location.href=`a5productedit.php?p_id=$rowid`' class='mdui-btn mdui-btn-icon mdui-color-teal-500'>
+                    <i class='mdui-icon material-icons'>brush</i>
+                </button>
+                <button onclick='location.href=`a5productedit.php?p_id=$rowid`' class='mdui-btn mdui-btn-icon mdui-color-red-500'>
+                    <i class='mdui-icon material-icons'>delete</i>
+                </button>
+                </td>";
+        }
+         // 定义表格右上角加号跳转目的地
+         $prodaddtarget="a5productedit.php?create=1";
 ?>
 
 <!DOCTYPE html>
@@ -35,9 +72,9 @@
     <!-- 主内容 -->
     <div class="content">
         <?php
-            echo "Product";
-        ?>
-        <button onClick="location.href='a5productedit.php?id=4'">跳到编辑页</button>
+            // 将开头定义的几个东西按顺序传进去，注意第四个参数是函数的名字
+            displayList($con,$productsql,$producthead,"productrender",$prodaddtarget);
+        ?>    
     </div>
 </body>
 <!-- 这个页面的JS，放在文档尾部 -->
